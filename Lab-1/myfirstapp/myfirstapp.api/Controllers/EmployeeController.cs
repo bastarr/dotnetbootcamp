346406b2d7c18e1d7a58a94cb7194944037673de
Brian Starr <brian.starr@outlook.com>
2020-02-29 12:17:32 -0500

    initial commit

---
+++ Lab-1/myfirstapp/myfirstapp.api/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using myfirstapp.core.domain;
+
+namespace myfirstapp.api.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class EmployeeController : ControllerBase
+   {
+       private static readonly string[] Names = new[]
+       {
+           "Bob", "Jim", "Fred", "Sally", "Linda", "Mary", "Tracy", "Taylor", "Steve", "David"
+       };
+
+       private readonly ILogger<EmployeeController> _logger;
+
+       public EmployeeController(ILogger<EmployeeController> logger)
+       {
+           _logger = logger;
+       }
+
+       [HttpGet]
+       public IEnumerable<Employee> Get()
+       {
+           var employees = new List<Employee>();
+           var rng = new Random();
+           return Enumerable.Range(1, 5).Select(index => {
+               var first = Names[rng.Next(Names.Length)];
+               return new Employee
+               {
+                   FirstName = first,
+                   LastName = "Smith",
+                   Phone = "(555)555-55-5555",
+                   Email = $"{first}.smith@test.com"
+               };
+           })
+           .ToArray();
+       }
+   }
+}
---
+++ Lab-1/myfirstapp/myfirstapp.core/domain/Employee.cs
@@ -0,0 +1,10 @@
+namespace myfirstapp.core.domain
+{
+   public class Employee
+   {
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
+       public string Phone { get; set; }
+       public string Email { get; set; }
+   }
+}
